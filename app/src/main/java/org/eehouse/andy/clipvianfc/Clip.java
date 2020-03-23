f46e525f83468cd03646f69f068c239c55bc38e2
Eric House <eehouse@eehouse.org>
2020-03-22 17:50:50 -0700

    show contents of (text) clipboard

---
+++ app/src/main/java/org/eehouse/andy/clipvianfc/Clip.java
@@ -0,0 +1,58 @@
+/* -*- compile-command: "find-and-gradle.sh inXw4dDeb"; -*- */
+/*
+ * Copyright 2020 by Eric House (xwords@eehouse.org).  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package org.eehouse.andy.clipvianfc;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import static android.content.ClipDescription.MIMETYPE_TEXT_PLAIN;
+
+public class Clip {
+    private final static String TAG = Clip.class.getSimpleName();
+
+    static String getData( Context context )
+    {
+        String pasteData = null;
+        ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        if ( clipboard.hasPrimaryClip()
+             && clipboard.getPrimaryClipDescription().hasMimeType(MIMETYPE_TEXT_PLAIN) ) {
+
+             ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
+             pasteData = item.getText().toString();
+
+             if ( null == pasteData ) {
+                 Uri pasteUri = item.getUri();
+                 if ( null != pasteUri ) {
+                     pasteData = resolve( pasteUri );
+                 }
+             }
+        }
+        return pasteData;
+    }
+
+    static String resolve( Uri uri )
+    {
+        Log.e( TAG, "resolve(" + uri + ") not doing anything" );
+        return null;
+    }
+}
--- app/src/main/java/org/eehouse/andy/clipvianfc/MainActivity.java
+++ app/src/main/java/org/eehouse/andy/clipvianfc/MainActivity.java
@@ -1,14 +1,64 @@
+/* -*- compile-command: "find-and-gradle.sh inXw4dDeb"; -*- */
+/*
+ * Copyright 2020 by Eric House (xwords@eehouse.org).  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package org.eehouse.andy.clipvianfc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.send).setOnClickListener( this );
+    }
+
+    @Override
+    protected void onPostResume()
+    {
+        super.onPostResume();
+        String clipData = Clip.getData(this);
+
+        EditText et = (EditText)findViewById(R.id.clip_text);
+        et.setText(clipData);
+        findViewById(R.id.send).setEnabled( clipData != null && clipData.length() > 0 );
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        int id = view.getId();
+        switch ( id ) {
+            case R.id.send:
+                Log.d( TAG, "send button pressed");
+                break;
+            default:
+                assert (false);
+        }
     }
 }
